9121d4186910d2c11e5297412217790107ca56b1
jinnian0703 <2234839477@qq.com>
2026-03-26 02:03:12 +0800

    chore: release 2.0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "top.jlen.vod"
         minSdk = 24
         targetSdk = 34
-        versionCode = 15
-        versionName = "2.0.1.1"
+        versionCode = 16
+        versionName = "2.0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
