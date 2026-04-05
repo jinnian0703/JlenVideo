c8ede50f929791af49c0a05fb296b83d7aa1f7ba
jinnian0703 <2234839477@qq.com>
2026-04-05 11:09:40 +0800

    chore: bump version to 2.1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "top.jlen.vod"
         minSdk = 24
         targetSdk = 34
-        versionCode = 23
-        versionName = "2.0.2.7"
+        versionCode = 24
+        versionName = "2.1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
